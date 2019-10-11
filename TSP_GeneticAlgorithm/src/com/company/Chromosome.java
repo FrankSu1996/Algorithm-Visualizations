28e16a06e7889451d81c1a5e8d4f9bc62be1d55c
Frank <franksu49@gmail.com>
2019-10-11 12:22:30 -0400

    added chromosome class, includes crossover and mutation methods

---
+++ TSP_GeneticAlgorithm/src/com/company/Chromosome.java
@@ -0,0 +1,109 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * Class to represent a chromosome in the genetic algorithm. For TSP, represents a possible
+ * path that connects all cities.
+ */
+public class Chromosome {
+
+    private final List<Gene> chromosome;
+
+    Chromosome(final List<Gene> chromosome) {
+        this.chromosome = Collections.unmodifiableList(chromosome);
+    }
+
+    /**
+     * Method to create initial chromosomes given a array of genes/points. Returns a single chromosome
+     * built from a random list of genes after shuffling.
+     * @param points an array of genes representing cities
+     * @return a chromosome for initial population
+     */
+    static Chromosome create(final Gene[] points) {
+        final List<Gene> genes = Arrays.asList(Arrays.copyOf(points, points.length));
+        Collections.shuffle(genes);
+        return new Chromosome(genes);
+    }
+
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        for(final Gene gene : this.chromosome) {
+            builder.append(gene.toString()).append((" : "));
+        }
+        return builder.toString();
+    }
+
+    List<Gene> getChromosome() {
+        return this.chromosome;
+    }
+
+    /**
+     * Method to return total distance of chromosome, represents it's fitness
+     * @return the total distance of chromosome. Essentially a random solution to TSP
+     */
+    double calculateDistance() {
+        double total = 0.0f;
+        for(int i = 0; i < this.chromosome.size() - 1; i++) {
+            total += this.chromosome.get(i).distance(this.chromosome.get(i+1));
+        }
+        return total;
+    }
+
+    /**
+     *
+     * @param other
+     * @return
+     */
+    Chromosome[] crossOver(final Chromosome other) {
+
+        //split chromosomes into two for both this and other chromosome
+        final List<Gene>[] myDNA = TSPUtils.split(this.chromosome);
+        final List<Gene>[] otherDNA = TSPUtils.split(other.getChromosome());
+
+        //first crossover starts with first "half" of myDNA
+        final List<Gene> firstCrossOver = new ArrayList<>(myDNA[0]);
+        //loop through other DNA list, if not contained already, add to myDNA
+        //this mimics random crossing over, as distance is calculated sequentially through the dna list
+        for(Gene gene : otherDNA[0]) {
+            if(!firstCrossOver.contains(gene)) {
+                firstCrossOver.add(gene);
+            }
+        }
+        for(Gene gene : otherDNA[1]) {
+            if(!firstCrossOver.contains(gene)) {
+                firstCrossOver.add(gene);
+            }
+        }
+
+        //repeat process for a second crossover
+        final List<Gene> secondCrossOver = new ArrayList<>(otherDNA[1]);
+        for(Gene gene : myDNA[0]) {
+            if(!secondCrossOver.contains(gene)) {
+                secondCrossOver.add(gene);
+            }
+        }
+
+        for(Gene gene : myDNA[1]) {
+            if(!secondCrossOver.contains(gene)) {
+                secondCrossOver.add(gene);
+            }
+        }
+
+        //check validity of crossover results
+        if(firstCrossOver.size() != TSPUtils.CITIES.length ||
+           secondCrossOver.size() != TSPUtils.CITIES.length) {
+            throw new RuntimeException("OOPS!");
+        }
+
+        return new Chromosome[] {new Chromosome(firstCrossOver), new Chromosome(secondCrossOver)};
+    }
+
+
+    Chromosome mutate() {
+
+    }
+}
