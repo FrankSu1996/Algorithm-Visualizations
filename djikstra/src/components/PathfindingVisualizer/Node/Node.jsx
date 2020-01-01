bc5e3e409f7cc1269251c63850e984913d88014b
Frank Su <franksu49@gmail.com>
2020-01-01 18:29:15 -0500

    set up grid

--- djikstra/src/components/PathfindingVisualizer/Node/Node.jsx
+++ djikstra/src/components/PathfindingVisualizer/Node/Node.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "./Node.css";
 
 class Node extends Component {
   state = {};
   render() {
-    return <div className="node"></div>;
+    return <div className="Node"></div>;
   }
 }
 
--- djikstra/src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ djikstra/src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import Node from "./Node/Node";
+import "./PathfindingVisualizer.css";
 
 class PathfindingVisualizer extends Component {
-  state = {};
+  state = {
+    nodes: []
+  };
+
+  //set up nodes for graph
+  componentDidMount() {
+    const nodes = [];
+    for (let row = 0; row < 20; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        currentRow.push([]);
+      }
+      nodes.push(currentRow);
+    }
+    this.setState({ nodes });
+  }
+
   render() {
+    const { nodes } = this.state;
+    console.log(nodes);
+
     return (
-      <div>
-        Foo
-        <Node></Node>
+      <div className="Grid">
+        {nodes.map((row, rowIdx) => {
+          return (
+            <div key={rowIdx}>
+              {row.map((node, nodeIdx) => (
+                <Node key={nodeIdx}></Node>
+              ))}
+            </div>
+          );
+        })}
       </div>
     );
   }
