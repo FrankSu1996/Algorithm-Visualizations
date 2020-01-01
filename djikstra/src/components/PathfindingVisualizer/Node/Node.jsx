981d2dce8975340354a9e743b4d4d1431976b789
Frank Su <franksu49@gmail.com>
2020-01-01 17:51:56 -0500

    initial project structure import

---
+++ djikstra/src/components/PathfindingVisualizer/Node/Node.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Node extends Component {
+  state = {};
+  render() {
+    return <div className="node"></div>;
+  }
+}
+
+export default Node;
---
+++ djikstra/src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Node from "./Node/Node";
+
+class PathfindingVisualizer extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        Foo
+        <Node></Node>
+      </div>
+    );
+  }
+}
+
+export default PathfindingVisualizer;
